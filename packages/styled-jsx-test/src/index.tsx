eb927d60a0c35a141630a3f6274f63d282f05da1
Masaki Hara <ackie.h.gmai@gmail.com>
2023-12-27 17:51:38 +0900

    Add styled-jsx test

---
+++ packages/styled-jsx-test/src/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default () => {
+  return <>
+    <button>Test</button>
+    <style jsx>{`
+      button {
+        background: red;
+      }
+    `}</style>
+  </>;
+};
