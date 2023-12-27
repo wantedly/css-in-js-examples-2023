35f9a49bd7526e1bd859c9486eea3afec416b9f9
Masaki Hara <ackie.h.gmai@gmail.com>
2023-12-27 18:16:42 +0900

    Set up TS

--- packages/styled-components-test/src/basic.test.tsx
+++ packages/styled-components-test/src/basic.test.tsx
@@ -1,5 +1,4 @@
 import { render } from "@testing-library/react";
-import React from "react";
 import { test, expect } from "@jest/globals";
 
 import App from "./basic";
--- packages/styled-jsx-test/src/index.test.tsx
+++ packages/styled-jsx-test/src/index.test.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import ReactDOMClient from "react-dom/client";
 import { act } from 'react-dom/test-utils';
 import { test, expect } from "@jest/globals";
