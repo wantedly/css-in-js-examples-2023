f69f91d830299913534643119502e57fa4479040
Masaki Hara <ackie.h.gmai@gmail.com>
2023-12-27 17:56:53 +0900

    Add emotion-test

---
+++ packages/emotion-test/src/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "@emotion/styled";
+
+const Button = styled.button`
+  background: red;
+`;
+
+export default () => {
+  return <Button>Test</Button>;
+};
